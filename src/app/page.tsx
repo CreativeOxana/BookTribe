6be6ad2d747aefe5cfd8cb3e91c189b252c6509b
Oxana <creativesouloxana@gmail.com>
2025-10-15 20:00:43 +0200

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const load = async () => {
+      const result = await fetch(
+        "https://openlibrary.org/search.json?q=harry+potter&limit=5"
+      );
+      console.log("🚀 ~ Home ~ result:", await result.json());
+    };
+
+    load();
+  }, []);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
